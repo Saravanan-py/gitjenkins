f3d8b92d240a9f83ebc1c7c61109d1aad33f578e
SaravananS30 <saravanan@vrdella.com>
2024-06-04 09:43:30 +0530

    modified

--- sample.py
+++ sample.py
@@ -2,3 +2,9 @@ print('hello world')
 print('hey im from github')
 print('hey im changed!')
 print('hey its neww message')
+arr=[1,4,5,3,2]
+for i in range(len(arr)-1):
+    for j in range(len(arr)-i-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+print(arr)
